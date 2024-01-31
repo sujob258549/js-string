d047bc185d4b49a85906260b4dca2fc3732749bf
DevSujon <sujonmaia274@gmail.com>
2024-01-31 12:13:49 +0600

    first commit

---
+++ change.js
@@ -0,0 +1,3 @@
+let change = 'Xampp name is Mamun';
+change = change.replace('X', 'Y');
+console.log(change);
---
+++ ned.js
@@ -0,0 +1,51 @@
+// let string = "A repository contains all project files, including the revision history. Already have a project repository elsewhere?";
+// let count = 0;
+
+// for (let i = 0; i < string.length; i++) {
+//     if (string[i] === 'a' || string[i] === 'A') {
+//         count++;
+//     }
+// }
+
+// console.log("The letter 'a' appears", count, "times in the string.");
+
+
+// let aNumber = 'A repository contains all project files, including the revision history. Already have a project repository elsewhere?';
+// let count = 0;
+
+// for (let i = 0; i < aNumber.length; i++){
+//     if(aNumber[i] === 'a' || aNumber[i] === 'A'){
+//         count++;
+//     }
+    
+// }
+// console.log(aNumber.includes("a"));
+
+// console.log("The letter 'A' occurs " + count + " times.");
+
+
+
+// let aNumber = 'A repository contains all project files, including the revision history. Already have a project repository elsewhere?';
+// let count = 0;
+
+// for (let i = 0; i < aNumber.length; i++) {
+//     if (aNumber[i] === 'a' || aNumber[i] === 'A') {
+//         count++;
+//     }
+// }
+
+// console.log("The letter 'A' occurs " + count + " times.");
+
+
+let word ='The invading Germanic tribes spoke similar languages, which in Britain developed into what we now call Old English. Old English did not sound or look like English today. Native English speakers now would have great difficulty understanding Old English. Nevertheless, about half of the most commonly used words in Modern English have Old English roots. The words be, strong and water, for example, derive from Old English. Old English was spoken until around 1100.';
+
+let count = 0;
+
+for (let i = 0; i < word.length; i++){
+    if(word[i] ===  'l' || word[i] === 'L'){
+        count++
+    }
+}
+console.log(word.includes('l'));
+console.log('Counts Number is A  :'+ count)
+
